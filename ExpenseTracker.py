6f4c2eb5ad646d261da6ca065e672ec26e761e6b
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2025-03-03 23:18:47 +0600

    Main Function and Expense Tracker Skeleton Ready

---
+++ ExpenseTracker.py
@@ -0,0 +1,14 @@
+class ExpenseTracker:
+    def add_expense():
+        pass
+
+    def view_expense():
+        pass
+
+    def filter_expense(category):
+        pass
+
+    def summarize_expenses():
+        pass
+    
+        
---
+++ main.py
@@ -0,0 +1,31 @@
+import ExpenseTracker
+
+def main():
+    tracker = ExpenseTracker.ExpenseTracker()
+
+    while True:
+        print("\nExpense Tracker")
+        print("1. Add Expense")
+        print("2. View Expense")
+        print("3. Filter By Category")
+        print("4. Exit")
+
+        choice = input("Choose an Option: ")
+
+        if choice == "1":
+            pass
+
+        elif choice == "2":
+            pass
+
+        elif choice == "3":
+            pass
+
+        elif choice == "4":
+            print("Exiting... Bye!!")
+            break
+
+        else:
+            print("\nInvalid Choice Try Again!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+main()
